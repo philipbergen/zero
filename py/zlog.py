ba38e06998ffd77c3f99996b7eb0f43d67c2efa2
Philip Bergen <philip.bergen@gmail.com>
2013-08-23 23:13:37 -1000

    Log line sender

---
+++ py/zlog.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+''' USAGE:
+      zlog [<config>] (lol|fyi|wtf|omg) <sender> (-|<message> <message>...)
+
+    <sender> is a logical name of emitting party.
+
+    If <message> is -, message lines are read from stdin.
+'''
+
+
+def log_format(config, sender, level, msg):
+    from json import dumps
+    from time import strftime
+    from socket import gethostname
+    return dumps([sender, gethostname(), level, strftime(config['ts-format']), msg])
+                 
+def main():
+    from sys import argv
+    from json import load
+    from itertools import imap
+    from collections import deque
+    from zero import zero, ZeroSetup
+    from os.path import dirname, exists
+    conf = dirname(__file__) + '/../log.json'
+    args = deque(argv[1:])
+    level = args.popleft()
+    if exists(level):
+        conf = level
+        level = args.popleft()
+    with open(conf) as fin:
+        conf = load(fin)['log']
+    sender = args.popleft()
+    if args[0] == '-':
+        messages = ZeroSetup.iter_stdin()
+    else:
+        messages = iter(args)
+    messages = imap(lambda x:log_format(conf, sender, level, x), messages)
+    z = zero(ZeroSetup('push', str(conf['port']), messages))
+    for msg in z:
+        pass
+
+if __name__ == '__main__':
+    main()
