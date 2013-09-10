e4d1d0a14a5c3104f056463a584b6a869338faf8
Philip Bergen <pbergen@salesforce.com>
2013-09-10 00:58:00 -0400

    More robust test in installer. Seems pythonpath is not needed.

--- install.sh
+++ install.sh
@@ -21,14 +21,17 @@ quiet_which () {
 ##
 # Returns true if $1 is an installed python module
 quiet_import () {
-    echo "import $1" | python >/dev/null 2>&1 && return 0
+    mod=$1
+    pip=$mod
+    [ "$mod" = zmq ] && pip=pyzmq
+    pip show $pip | grep -q '^Location:' && return 0
+    echo "import $mod" | python >/dev/null 2>&1 && return 0
     return 1
 }
 
 ##
-# Installs pip and then goes on to use pip to install required 
-# python modules.
-install_libs() {
+# Makes sure there is pip and python on the system
+install_pip() {
     quiet_which python || {
         echo "*** ERROR: python is required"
         exit 1
@@ -40,6 +43,11 @@ install_libs() {
         sudo python get-pip.py
         rm -f get-pip.py*
     }
+}
+
+##
+# With pip install required python modules.
+install_libs() {
     quiet_import zmq || {
         echo "*** INFO: Installing missing python module pyzmq"
         pip install --egg --user --install-option=--zmq=bundled --install-option=--prefix= pyzmq
@@ -87,6 +95,7 @@ EOF
 
 cd "$here"
 rm -f env.py* py/docopt.py*
+install_pip
 create_env
 . ./env.sh
 install_libs
